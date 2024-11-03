b26a454bd866e9dc948b8aa911a0f17b960595b3
valeria shishkina <va1cone@yandex.ru>
2024-11-03 21:35:41 +0300

    correction of critical moment

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,4 +1,3 @@
 import random
-#генератор новых почт для теста
 
-new_email = f"valeria_shishkina_13{random.randint(100, 9999)}@yandex.ru" #создание новой почты
+new_email = f"valeria_shishkina_13{random.randint(100, 999999)}@yandex.ru" #создание новой почты
--- tests/test_creating_a_user.py
+++ tests/test_creating_a_user.py
@@ -1,23 +1,45 @@
+import pytest
 import requests
 from helpers import *
 import allure
 
 URL = "https://stellarburgers.nomoreparties.site/api/auth/register"
+URL_2 = "https://stellarburgers.nomoreparties.site/api/auth/login"
+URL_3 = "https://stellarburgers.nomoreparties.site/api/auth/user"
 
 test_data = {
     "email": new_email,
     "password": "cool password",
     "name": "Lerochka"
 }
+
+@pytest.fixture(scope="function")
+def user_registration_and_delete():
+    response = requests.post(URL, json=test_data)
+    assert response.status_code == 200
+    assert response.json()["success"] is True
+
+    access_token = response.json()["accessToken"]
+    yield access_token
+
+    delete_response = requests.delete(URL_3, headers={"Authorization": access_token})
+    assert delete_response.status_code == 202
+    assert delete_response.json()["success"] is True
+
+
 class TestUserRegistration:
 
     @allure.title("Проверка возможности зарегистрировать пользователя")
-    def test_user_registration(self):
-        response = requests.post(URL, json=test_data)
-        assert response.status_code == 200
-        assert response.json()["success"] is True
-        assert response.json()["user"]["email"] == test_data["email"]
-        assert response.json()["user"]["name"] == test_data["name"]
+    def test_user_registration(self, user_registration_and_delete):
+
+        login_response = requests.post(URL_2, json={
+            "email": test_data["email"],
+            "password": test_data["password"]
+        })
+
+        assert login_response.status_code == 200
+        assert login_response.json()["user"]["email"] == test_data["email"]
+        assert login_response.json()["user"]["name"] == test_data["name"]
 
     @allure.title("Проверка возможности зарегистрировать уже зарегистрированного пользователя")
     def test_creating_registered_user(self):
